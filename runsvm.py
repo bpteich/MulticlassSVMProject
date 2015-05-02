832534681cbaec0b56c06b4fc0fc8a1de39dec34
Brendan Teich <bpteich@gmail.com>
2015-05-02 16:52:27 -0400

    Can now score rbf_SVC.  Working on getting 1v1 and 1vsAll working.

--- runsvm.py
+++ runsvm.py
@@ -12,11 +12,11 @@ from sklearn.preprocessing import LabelEncoder
 '''Returns the average score of the given classifier'''
 def score(estimator, trainFeatures, trainTargets):
 	kfold = 5
-	scores = cross_validation.cross_val_score(estimator, trainFeatures, trainTargets, cv=kfold, n_jobs=-1)
+	scores = cross_validation.cross_val_score(estimator, trainFeatures, trainTargets, cv=kfold, n_jobs=1)
 	avg = 0
 	for i in range(len(scores)):
 		avg = avg + scores[i]/len(scores)
-
+	return avg
 
 def onevsall(estimator):
 	return sklearn.multiclass.OneVsRestClassifier(estimator, n_jobs=-1)
@@ -25,14 +25,27 @@ def onevsone(estimator):
 	return sklearn.multiclass.OneVsOneClassifier(estimator, n_jobs=-1)
 
 
+def quickScore(estimator, trainFeatures, trainTargets):
+	feat_train, feat_test, tar_train, tar_test = cross_validation.train_test_split(trainFeatures, trainTargets, train_size=1000, test_size=10)
+	return score(estimator, feat_train, tar_train)
+
+'''
+def score(estimator, test):
+	return estimator.decision_function(test)
+	'''
+
 if __name__ == '__main__':
 	trainSet = pandas.read_csv("train.csv")
 	trainTargets = LabelEncoder().fit_transform(trainSet['target'])
 	trainFeatures = trainSet.drop('target', axis=1)
-	trainFeatures = trainFeatures.T.to_dict().values()
- 	rbf_SVC = SVC(C=1.0, kernel='rbf', gamma='2.0')
-	vsone_rbf_score = score(onevsone(rbf_SVC), trainFeatures, trainTargets)
-	print vsone_rbf_score
+	#trainFeatures[trainFeatures > 4] = 4
+	test = pandas.read_csv("test.csv") 
+	#trainFeatures = trainFeatures.T.to_dict().values()
+ 	rbf_SVC = SVC(C=1.0, kernel='rbf', gamma=2.0)
+	#rbf_1v1 = onevsone(rbf_SVC).fit(trainFeatures,trainTargets)
+	#poly_SVC = SVC(C=1.0, kernel='poly',degree=3)
 
+	result = quickScore(rbf_SVC, trainFeatures, trainTargets)
+	print result
 
 
