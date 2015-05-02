7313b20549200c06e9c700a32488962c84919237
Brendan Teich <bpteich@gmail.com>
2015-05-02 16:09:13 -0400

    Fixed a typerror in the code for the traingFeatures

--- runsvm.py
+++ runsvm.py
@@ -1,21 +1,18 @@
 import pandas
 import numpy
-import sklearn.cross_validation
+from sklearn import cross_validation
 import sklearn.multiclass
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 
 
-trainSet = pandas.read_csv("train.csv")
-trainTargets = LabelEncoder().fit_transform(trainSet['target'])
-trainFeatures = trainSet.drop('target', axis=1)
 
 
 
 '''Returns the average score of the given classifier'''
-def score(estimator):
-	numFolds = 5
-	scores = sklearn.cross_validation.cross_val_score(estimator, trainFeatures, y=trainTargets, scoring=None, cv=numFolds, n_jobs=-1, verbose=0, fit_params=None, pre_dispatch='2*n_jobs')
+def score(estimator, trainFeatures, trainTargets):
+	kfold = 5
+	scores = cross_validation.cross_val_score(estimator, trainFeatures, trainTargets, cv=kfold, n_jobs=-1)
 	avg = 0
 	for i in range(len(scores)):
 		avg = avg + scores[i]/len(scores)
@@ -27,6 +24,15 @@ def onevsall(estimator):
 def onevsone(estimator):
 	return sklearn.multiclass.OneVsOneClassifier(estimator, n_jobs=-1)
 
-rbf_SVC = SVC(C=1.0, kernel='rbf', gamma='0.0')
+
+if __name__ == '__main__':
+	trainSet = pandas.read_csv("train.csv")
+	trainTargets = LabelEncoder().fit_transform(trainSet['target'])
+	trainFeatures = trainSet.drop('target', axis=1)
+	trainFeatures = trainFeatures.T.to_dict().values()
+ 	rbf_SVC = SVC(C=1.0, kernel='rbf', gamma='2.0')
+	vsone_rbf_score = score(onevsone(rbf_SVC), trainFeatures, trainTargets)
+	print vsone_rbf_score
+
 
 
